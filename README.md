78e7c57f50518f7bcfceb59bfeb5a1693be87e1a
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-01-08 10:42:50 -0600

    Add README and enhance Main class with user input functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+# Programación orientada a objetos
+
+La programación orientada a objetos
+es un paradigma de programación que
+permite modelar objetos del mundo real
+en un programa de computadora.
+
+## Lenguajes de programación orientada a objetos
--- TiposDeDatos/src/Main.java
+++ TiposDeDatos/src/Main.java
@@ -6,10 +6,29 @@ public class Main {
         float numberFloat = 3.2f;
         String word = "Hello Angel";
 
+        // Definir un objeto de la clase Scanner
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Numero entero: " + number);
-        System.out.println("Numero flotante: " + numberFloat);
         System.out.println("Palabra: " + word);
+        System.out.println("Numero flotante: " + numberFloat);
+
+        // Leer un entero
+        System.out.println("Ingrese un numero entero:");
+        number = sc.nextInt();
+
+        // Leer un flotante
+        System.out.println("Ingresa un numero con punto decimal: ");
+        numberFloat = sc.nextFloat();
+
+
+        // Leer una frase
+        sc.nextLine();
+        System.out.println("Ingresa una palabra:");
+        word = sc.nextLine();
+
+        System.out.println("Numero entero: " + number);
+        System.out.println("Palabra: " + word);
+        System.out.println("Numero flotante: " + numberFloat);
     }
 }
