2235d5bd25340fb99c3d28b6ce4b5fabfb81fc98
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-01-08 10:21:49 -0600

    Add .gitignore files and initial Main class implementation

---
+++ TiposDeDatos/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 0;
+        float numberFloat = 3.2f;
+        String word = "Hello Angel";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Numero entero: " + number);
+        System.out.println("Numero flotante: " + numberFloat);
+        System.out.println("Palabra: " + word);
+    }
+}
