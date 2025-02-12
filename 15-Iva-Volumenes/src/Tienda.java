d8dd7610a757c012f955672d5c2cd3ef2e0f5688
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-02-12 13:38:42 -0600

    add project

---
+++ 15-Iva-Volumenes/src/Tienda.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tienda  implements Input{
+    public void readData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indrodu");
+
+    }
+}
